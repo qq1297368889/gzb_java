34cc2ba4e408ea8aa299dc1a61846706ed4b72bb
userName <1297368889@qq.com>
2025-09-01 00:06:52 +0800

    1.0.0
    Signed-off-by: userName <1297368889@qq.com>

--- test_spring_mvc/src/main/java/gzb/test_spring_mvc/demo/BasicController.java
+++ test_spring_mvc/src/main/java/gzb/test_spring_mvc/demo/BasicController.java
@@ -28,6 +28,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BasicController {
 
+    // http://127.0.0.1:8080/test/test1?name=lisi
+    @RequestMapping("/test/test1")
+    @ResponseBody
+    public String test1(String name) {
+        return "{\"code\":\"1\"\"name\":\""+name+"\"}";
+    }
 
     // http://127.0.0.1:8080/user
     @RequestMapping("/user")
