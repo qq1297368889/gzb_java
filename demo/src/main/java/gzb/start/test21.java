1069ea42db91930b3234523012202075403913ef
userName <1297368889@qq.com>
2025-10-30 02:44:01 +0800

    25.10.30B

--- demo/src/main/java/gzb/start/test21.java
+++ demo/src/main/java/gzb/start/test21.java
@@ -1,11 +1,6 @@
 package gzb.start;
 
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONWriter;
-import com.frame.dao.SysUsersDao;
-import com.frame.dao.impl.SysUsersDaoImpl;
-import com.frame.entity.SysUsers;
-import gzb.tools.DateTime;
 import gzb.tools.Tools;
 import gzb.tools.log.Log;
 import gzb.tools.log.LogImpl;
@@ -17,14 +12,6 @@ import java.util.List;
 public class test21 {
     public static void main(String[] args) throws Exception {
         Log log =new LogImpl();
-        SysUsers sysUsers = new SysUsers();
-        SysUsersDao sysUsersDao=new SysUsersDaoImpl();
-        sysUsers.setSysUsersId(1L).setSysUsersAcc(Tools.getRandomString(12));
-        sysUsersDao.saveAsync(sysUsers,()->{
-            log.w("保存失败回调执行了",sysUsers);
-        });
-        Tools.sleep(1000);
-        sysUsersDao.saveAsync(sysUsers);
 
     }
 }
