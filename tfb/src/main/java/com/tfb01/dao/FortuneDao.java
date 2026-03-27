6cd9f42e14c9a74c593a22f613323e2ad6766e85
userName <1297368889@qq.com>
2026-03-27 18:57:23 +0800

    26.03.27-A

--- tfb/src/main/java/com/tfb01/dao/FortuneDao.java
+++
@@ -1,7 +0,0 @@
-package com.tfb01.dao;
-
-import com.tfb01.entity.Fortune;
-import gzb.frame.db.BaseDao;
-
-public interface FortuneDao extends BaseDao<Fortune> {
-}
--- tfb/src/main/java/com/tfb01/entity/Message.java
+++
@@ -1,5 +0,0 @@
-package com.tfb01.entity;
-
-public class Message {
-    public String message;
-}
